ea8ec63ff5803e3011c8b20b9efde0b77c4a7b40
John D'Argenio <jpdarg6@gmail.com>
2018-06-15 20:49:18 -0400

    wow

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import haha from './Chilis.mp4';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+     {/*     <img src={logo} className="App-logo" alt="logo" />  */}
+      {/*    <h1 className="App-title">Welcome to React</h1>   */}
+	  <video autoPlay preload="auto" className="funnyy" ref="vidd" id="funny" width="1280" height="720" controls>
+		<source src={haha} type ='video/mp4' id="work"  />
+	</video>
+		<p className="dummy"> You are a dummy </p>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          HAHA
         </p>
       </div>
     );
   }
 }
-
 export default App;
